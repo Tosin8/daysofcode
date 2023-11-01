432c4cf99aafcb76b9bd5c1df573c92c9e122a04
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-01 14:24:25 +0100

    Creating Splash bkg

--- lib/day 1/travel_home.dart
+++ lib/day 1/travel_home.dart
@@ -6,10 +6,21 @@ class TravelHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: AssetImage(image:)),
-      )
-    );
+        body: Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/travel_ui/1.jpg'),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Container(
+          decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xff635050), Color(0x00251e1e)],
+          begin: Alignment(0.00, -1.00),
+          end: Alignment(0, 1),
+        ),
+      )),
+    ));
   }
 }
