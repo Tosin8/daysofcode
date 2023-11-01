86c32e0cae9cadb1f7a779c4fb7ab108add87c7d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-01 18:09:28 +0100

    Refactor to Stateful for Vertical Card

--- lib/day 1/travel_discover.dart
+++ lib/day 1/travel_discover.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class TravelDiscover extends StatelessWidget {
+class TravelDiscover extends StatefulWidget {
   const TravelDiscover({super.key});
 
   @override
+  State<TravelDiscover> createState() => _TravelDiscoverState();
+}
+
+class _TravelDiscoverState extends State<TravelDiscover> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Discover Page")));
+    return const SafeArea(
+        child: Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 30),
+        child: Column(
+          children: [
+            Text(
+              'Discover',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+            ),
+            Text(
+                'One\'s destination is never a place, but \nalways a new way of seeing things.')
+          ],
+        ),
+      ),
+    ));
   }
 }
