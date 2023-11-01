9ad4f37860bf23ed2752e9452c6d6c06e9642cbe
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-01 15:28:29 +0100

    Update travel_discover.dart

--- lib/day 1/travel_discover.dart
+++ lib/day 1/travel_discover.dart
@@ -5,6 +5,6 @@ class TravelDiscover extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(body: Center(child: Text("Discover Page")));
   }
 }
