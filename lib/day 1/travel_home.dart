8f57002ce01782e9af3d7af1c5fc9e734188f865
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-01 14:13:37 +0100

    adding landing page image

--- lib/day 1/travel_home.dart
+++ lib/day 1/travel_home.dart
@@ -5,6 +5,11 @@ class TravelHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: AssetImage(image:)),
+      )
+    );
   }
 }
