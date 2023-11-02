ed029dd172e9d5e5a5b19c944879f2452430dfaf
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-02 15:19:30 +0100

    Updating Scaffold Header

--- lib/day 2/interior_home.dart
+++ lib/day 2/interior_home.dart
@@ -5,6 +5,31 @@ class InteriorHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: const Drawer(),
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15),
+            child: Container(
+              width: 37,
+              height: 37,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color(0xffd9d9d9),
+              ),
+              child: const Center(
+                  child: Text(
+                '0',
+                style: TextStyle(fontWeight: FontWeight.w700),
+              )),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:daysofcode/day%202/interior_home.dart';
 import 'package:flutter/material.dart';
 
-import 'day 1/travel_home.dart';
-
 void main() {
   runApp(const MyApp());
 }
