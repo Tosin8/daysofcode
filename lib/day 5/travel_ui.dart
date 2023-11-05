f68cc82598aeca72c4cfa4d2d50982ea562b23cc
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-05 18:23:56 +0100

    Updating with TextField at the header

--- lib/day 5/travel_ui.dart
+++ lib/day 5/travel_ui.dart
@@ -6,12 +6,49 @@ class TravelUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: NetworkImage(
-                  'https://unsplash.com/photos/flat-ray-photography-of-book-pencil-camera-and-with-lens-3OiYMgDKJ6k'),
-              fit: BoxFit.cover)),
-    ));
+        backgroundColor: Colors.grey[100],
+        body: SafeArea(
+            child: Container(
+          width: double.infinity,
+          height: 300,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/travel_ui/header.jpg'),
+                  fit: BoxFit.cover)),
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Colors.black.withOpacity(0.6),
+              Colors.black.withOpacity(.1)
+            ], begin: Alignment.bottomCenter)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'What would you like to find? ',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.search, color: Colors.grey),
+                        hintStyle: TextStyle(color: Colors.grey, fontSize: 13),
+                        hintText: 'Search for Cities'),
+                  ),
+                ),
+                SizedBox(height: 30),
+              ],
+            ),
+          ),
+        )));
   }
 }
