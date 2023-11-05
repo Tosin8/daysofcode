238e109edfcca9188b2dc090190527a1705e4bc7
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-05 19:31:54 +0100

    Adding Fade Animation

---
+++ lib/day 5/fade_animation.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+  const FadeAnimation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([])
+    return ControlledAnimation();
+  }
+}
--- lib/day 5/travel_ui.dart
+++ lib/day 5/travel_ui.dart
@@ -8,92 +8,130 @@ class TravelUI extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.grey[100],
         body: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 300,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/travel_ui/header.jpg'),
-                        fit: BoxFit.cover)),
-                child: Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                    Colors.black.withOpacity(0.6),
-                    Colors.black.withOpacity(.1)
-                  ], begin: Alignment.bottomCenter)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      const Text(
-                        'What would you like to find? ',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700),
-                      ),
-                      const SizedBox(height: 10),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 40),
-                        padding: const EdgeInsets.symmetric(vertical: 3),
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.white),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              prefixIcon:
-                                  Icon(Icons.search, color: Colors.grey),
-                              hintStyle:
-                                  TextStyle(color: Colors.grey, fontSize: 13),
-                              hintText: 'Search for Cities, places...'),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 300,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/travel_ui/header.jpg'),
+                          fit: BoxFit.cover)),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                      Colors.black.withOpacity(0.6),
+                      Colors.black.withOpacity(.1)
+                    ], begin: Alignment.bottomCenter)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'What would you like to find? ',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700),
                         ),
-                      ),
-                      const SizedBox(height: 30),
-                    ],
+                        const SizedBox(height: 10),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 40),
+                          padding: const EdgeInsets.symmetric(vertical: 3),
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Colors.white),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                prefixIcon:
+                                    Icon(Icons.search, color: Colors.grey),
+                                hintStyle:
+                                    TextStyle(color: Colors.grey, fontSize: 13),
+                                hintText: 'Search for Cities, places...'),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Best Destination',
-                        style: TextStyle(
-                            color: Colors.grey[800],
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Container(
-                        height: 200,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: [
-                            carousel(
-                                image: 'assets/travel_ui/rome.jpg',
-                                title: 'Travel'),
-                            carousel(
-                                image: 'assets/travel_ui/rio.jpg',
-                                title: 'Travel'),
-                            carousel(
-                                image: 'assets/travel_ui/tokyo.jpg',
-                                title: 'Travel'),
-                          ],
+                const SizedBox(height: 20),
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Best Destination',
+                          style: TextStyle(
+                              color: Colors.grey[800],
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
                         ),
-                      )
-                    ],
+                        SizedBox(height: 10),
+                        Container(
+                          height: 200,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              carousel(
+                                  image: 'assets/travel_ui/rome.jpg',
+                                  title: 'Rome'),
+                              carousel(
+                                  image: 'assets/travel_ui/rio.jpg',
+                                  title: 'Rio'),
+                              carousel(
+                                  image: 'assets/travel_ui/tokyo.jpg',
+                                  title: 'Tokyo'),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const Text('Favorites'),
-            ],
+                const SizedBox(height: 20),
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Best Hotels',
+                          style: TextStyle(
+                              color: Colors.grey[800],
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          height: 200,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              carousel(
+                                  image: 'assets/travel_ui/rome.jpg',
+                                  title: 'Rome'),
+                              carousel(
+                                  image: 'assets/travel_ui/rio.jpg',
+                                  title: 'Rio'),
+                              carousel(
+                                  image: 'assets/travel_ui/tokyo.jpg',
+                                  title: 'Tokyo'),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ));
   }
@@ -108,19 +146,21 @@ class TravelUI extends StatelessWidget {
               image:
                   DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
           child: Container(
+            padding: const EdgeInsets.all(30),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 gradient: LinearGradient(colors: [
                   Colors.black.withOpacity(.8),
                   Colors.black.withOpacity(.2),
                 ], begin: Alignment.bottomRight)),
-            child: Center(
+            child: Align(
+              alignment: Alignment.bottomLeft,
               child: Text(
                 title,
                 style: TextStyle(
                     color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15),
               ),
             ),
           )),
