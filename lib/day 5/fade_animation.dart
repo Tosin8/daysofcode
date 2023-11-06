9b5156fff1b48c739171f98075cdafba3c88f74e
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-06 19:28:14 +0100

    Updating Fade Animation

--- lib/day 5/fade_animation.dart
+++ lib/day 5/fade_animation.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class FadeAnimation extends StatelessWidget {
   final double delay;
   final Widget child;
-  const FadeAnimation({super.key});
+  //const FadeAnimation({super.key});
+  FadeAnimation(this.delay, this.child); 
+
 
   @override
   Widget build(BuildContext context) {
