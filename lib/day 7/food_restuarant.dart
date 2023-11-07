4497c02613f6cc94af07bc107431104f1d3072bb
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-07 16:45:50 +0100

    Updating Ui for Restaruant

---
+++ lib/day 7/food_restuarant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RestaurantUI extends StatelessWidget {
+  const RestaurantUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:daysofcode/day%201/travel_home.dart';
 import 'package:daysofcode/day%202/interior_home.dart';
 import 'package:daysofcode/day%205/travel_ui.dart';
+import 'package:daysofcode/day%207/food_restuarant.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TravelUI(),
+      home: const RestaurantUI(),
     );
   }
 }
