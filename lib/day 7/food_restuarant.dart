34b0cf4aab65fcda97cc36a2f233bfc2fdf22e44
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-08 14:52:06 +0100

    Update food_restuarant.dart

--- lib/day 7/food_restuarant.dart
+++ lib/day 7/food_restuarant.dart
@@ -33,7 +33,7 @@ class RestaurantUI extends StatelessWidget {
                         color: Colors.white,
                         fontSize: 40,
                         fontWeight: FontWeight.w700),
-                  ).animate().fadeIn(duration: 500.ms),
+                  ).animate().fadeIn(duration: Duration(milliseconds: 3000)),
                 ),
                 const SizedBox(height: 20),
                 const Row(
