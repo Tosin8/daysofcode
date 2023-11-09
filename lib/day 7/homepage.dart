caa8957f5c9ff1770a475e9929905d5c0d4ecc01
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-09 16:42:57 +0100

    refactor

--- lib/day 7/homepage.dart
+++ lib/day 7/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_fadein/flutter_fadein.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,7 +12,7 @@ class HomePage extends StatelessWidget {
           backgroundColor: Colors.grey[100],
           elevation: 0,
           systemOverlayStyle: SystemUiOverlayStyle.dark,
-          actions: [
+          actions: const [
             Icon(Icons.shopping_bag_outlined),
           ],
           automaticallyImplyLeading: false,
@@ -19,7 +20,26 @@ class HomePage extends StatelessWidget {
         body: SafeArea(
             child: Column(
           children: [
-            Padding(padding: const EdgeInsets.symmetric(horizontal: 20.0))
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    const FadeIn(
+                      child: Text(
+                        'Food Delivery',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.w500),
+                      ),
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 50,
+                      child: ListView(scrollDirection: Axis.horizontal),
+                    )
+                  ],
+                ))
           ],
         )));
   }
