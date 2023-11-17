a967bef3dbd56e1c6b622cdb7656e2a79e62c11b
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-17 23:21:11 +0100

    Creating Parrallax Carousel

--- lib/day8.dart
+++ lib/day8.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parallax/parallax.dart';
 
 class day8 extends StatefulWidget {
   const day8({super.key});
@@ -8,6 +9,12 @@ class day8 extends StatefulWidget {
 }
 
 class _day8State extends State<day8> {
+  final List<String> listOfImages = [
+    'assets/travel_ui/1.jpg',
+    'assets/travel_ui/sydney/main.jpg',
+    'assets/travel_ui/rio.jpg',
+    'assets/travel_ui/tokyo.jpg'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +22,17 @@ class _day8State extends State<day8> {
         title: Text('Parallax'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+          child: Parallax(
+        parallaxImages: listOfImages,
+        // skewAlpha: 3.0,
+        // skewBeta: 7.0,
+        height: 300.0,
+        width: 500.0,
+        paddingHorizontal: 10.0,
+        viewportFraction: 0.8,
+        circularBorder: true,
+      )),
     );
   }
 }
