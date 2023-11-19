918ef5e66fc0270a5e34a8a360c7117fb499d6a2
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-19 18:09:55 +0100

    Update movie_card.dart

---
+++ lib/day 18/movie_ui/widgets/movie_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../model/movie.dart';
+
+class MovieCardWidget extends StatelessWidget {
+  final Movie movie;
+  const MovieCardWidget({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
