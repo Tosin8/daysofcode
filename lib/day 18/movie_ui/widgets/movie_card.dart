4749abec42f9bd2583558d8bc62b48a89e95f4bc
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-19 18:14:49 +0100

    Update movie_card.dart

--- lib/day 18/movie_ui/widgets/movie_card.dart
+++ lib/day 18/movie_ui/widgets/movie_card.dart
@@ -7,7 +7,20 @@ class MovieCardWidget extends StatelessWidget {
   const MovieCardWidget({super.key, required this.movie});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          child: Column(
+            children: [
+              Expanded(child: buildImage(movie: movie)),
+            ],
+          ),
+        ),
+      );
 }
