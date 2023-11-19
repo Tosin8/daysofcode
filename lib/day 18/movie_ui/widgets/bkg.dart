e7626cd10c600ed002037bf332445290f1dda712
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-19 17:45:47 +0100

    Update bkg.dart

--- lib/day 18/movie_ui/widgets/bkg.dart
+++ lib/day 18/movie_ui/widgets/bkg.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
+import '../data/movies.dart';
+import '../model/movie.dart';
+
 class BkgWidget extends StatelessWidget {
-  const BkgWidget({super.key});
+  final PageController controller;
+  const BkgWidget({super.key, required this.controller});
 
   @override
-  Widget build(BuildContext context) {
-    return PageView.builder(
-      reverse: true,
-      physics: NeverScrollableScrollPhysics(),
-    );
-  }
+  Widget build(BuildContext context) => PageView.builder(
+        reverse: true,
+        physics: NeverScrollableScrollPhysics(),
+        controller: controller,
+        itemBuilder: (context, index) {
+          final movie = movies[index];
+
+          return buildBackground(movie: movie);
+        },
+        itemCount: movies.length,
+      );
+  Widget buildBackground({required Movie movie}) => Stack(
+        children: [
+          Container(
+              height: double.infinity,
+              width: double.infinity,
+              child: Image.asset(
+                movie.imageUrl,
+                fit: BoxFit.cover,
+              )),
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.white.withOpacity(0.0001), Colors.white])),
+          )
+        ],
+      );
 }
