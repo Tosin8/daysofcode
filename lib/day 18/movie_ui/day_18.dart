e7c5174eadb29e40b4554740666e119cf5547ee1
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-19 17:17:10 +0100

    Update day_18.dart

--- lib/day 18/movie_ui/day_18.dart
+++ lib/day 18/movie_ui/day_18.dart
@@ -9,13 +9,19 @@ class day18 extends StatelessWidget {
         body: Padding(
       padding: const EdgeInsets.all(5.0),
       child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-               topRight: Radius.circular(30)),
-        ),
-      ),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Column(
+              children: [
+                Expanded(child: buildImage(movie: movie)),
+              ],
+            ),
+          )),
     ));
   }
 }
