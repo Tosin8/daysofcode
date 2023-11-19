f8f08e86560f7f82ef13042da8dd7ee77bb30172
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-19 20:03:46 +0100

    Refactoring

--- lib/day 18/movie_ui/day_18.dart
+++ lib/day 18/movie_ui/day_18.dart
@@ -1,33 +1,52 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+import 'data/movies.dart';
+import 'widgets/bkg.dart';
+import 'widgets/buy_button.dart';
+import 'widgets/movie_card.dart';
+
 class day18 extends StatelessWidget {
-  const day18({super.key});
+  final controller = PageController();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-            child: Column(
-              children: [
-                Expanded(child: buildImage(movie: movie)),
-                const SizedBox(height: 4),
-                Text(
-                  movie.movieName,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )
-              ],
+  Widget build(BuildContext context) => Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          leading: Icon(Icons.close),
+          actions: [Icon(Icons.person_outline), SizedBox(width: 15)],
+        ),
+        body: Stack(
+          children: [
+            BkgWidget(controller: controller),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CarouselSlider(
+                    items:
+                        movies.map((e) => MovieCardWidget(movie: e)).toList(),
+                    options: CarouselOptions(
+                      enableInfiniteScroll: false,
+                      viewportFraction: 0.75,
+                      height: MediaQuery.of(context).size.height * 0.7,
+                      enlargeCenterPage: true,
+                      onPageChanged: (index, _) => controller.animateToPage(
+                        index,
+                        duration: Duration(seconds: 1),
+                        curve: Curves.ease,
+                      ),
+                    ),
+                  ),
+                  BuyBtnWidget(),
+                  SizedBox(height: 20),
+                ],
+              ),
             ),
-          )),
-    ));
-  }
+          ],
+        ),
+      );
 }
