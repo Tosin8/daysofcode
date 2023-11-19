3c7d2009c2b2ef9152c43e49f930271cd9c35984
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-19 17:47:21 +0100

    Update buy_button.dart

---
+++ lib/day 18/movie_ui/widgets/buy_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BuyBtnWidget extends StatelessWidget {
+  const BuyBtnWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200, height: 40, 
+      child: R,
+    );
+  }
+}
