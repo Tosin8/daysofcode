cc000cfa1d46f49fa456e5eb7374a32b99d07043
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-20 21:19:04 +0100

    Update category.dart

--- lib/day 19/ecommerce/category.dart
+++ lib/day 19/ecommerce/category.dart
@@ -10,6 +10,37 @@ class Category extends StatefulWidget {
 class _CategoryState extends State<Category> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(right: 20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image:
+                  DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [
+                Colors.black.withOpacity(0.8),
+                Colors.black.withOpacity(0.2),
+              ], begin: Alignment.bottomRight),
+            ),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                title,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                    fontSize: 18),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
