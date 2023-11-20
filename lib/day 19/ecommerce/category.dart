45ef31446a057738630048f05646139a4246700e
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-20 22:27:52 +0100

    Update category.dart

--- lib/day 19/ecommerce/category.dart
+++ lib/day 19/ecommerce/category.dart
@@ -17,84 +17,92 @@ class Category extends StatefulWidget {
 class _CategoryState extends State<Category> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: SingleChildScrollView(
-          child: Column(
-        children: [
-          Hero(
-            tag: widget.tag,
-            child: Material(
-              child: Container(
-                height: 300,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage(widget.image), fit: BoxFit.cover)),
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            Hero(
+              tag: widget.tag,
+              child: Material(
                 child: Container(
-                  padding: EdgeInsets.all(10),
+                  height: 250,
                   decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                      Colors.black.withOpacity(0.8),
-                      Colors.black.withOpacity(0.1),
-                    ], begin: Alignment.bottomRight),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          SizedBox(height: 30),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              IconButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        PageTransition(
-                                            child: const Shop(),
-                                            type: PageTransitionType.fade));
-                                  },
-                                  icon: const Icon(Icons.arrow_back_ios,
-                                      color: Colors.white)),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.search,
-                                          color: Colors.white)),
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(
-                                          Icons.favorite_border_outlined,
-                                          color: Colors.white)),
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.shopping_bag,
-                                          color: Colors.white)),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      Text(
-                        widget.title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                            fontSize: 30),
-                      ),
-                      SizedBox(height: 50),
-                    ],
+                      image: DecorationImage(
+                          image: AssetImage(widget.image), fit: BoxFit.cover)),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.black.withOpacity(0.8),
+                        Colors.black.withOpacity(0.1),
+                      ], begin: Alignment.bottomRight),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            SizedBox(height: 30),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          PageTransition(
+                                              child: const Shop(),
+                                              type: PageTransitionType.fade));
+                                    },
+                                    icon: const Icon(Icons.arrow_back_ios,
+                                        color: Colors.white)),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.search,
+                                            color: Colors.white)),
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                            Icons.favorite_border_outlined,
+                                            color: Colors.white)),
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.shopping_bag,
+                                            color: Colors.white)),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Text(
+                          widget.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                              fontSize: 30),
+                        ),
+                        SizedBox(height: 50),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
-      )),
+            Padding(
+              padding: const EdgeInsets.all(2),
+              child: Column(
+                children: [Text('New Product')],
+              ),
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
